464b1a313806455c6a8aaa003ff7ee3ebf8451c1
luthanhar <82824823+luthanhar@users.noreply.github.com>
2021-12-22 17:34:41 +0530

    Delete camp - 12 July 2017 directory

--- camp - 12 July 2017/live/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$url = "http://hrishirt.cse.iitk.ac.in/raktarpan/live/index.php";
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-	$output = curl_exec($curl);
-	curl_close($curl);
-
-	echo str_replace('./', 'http://hrishirt.cse.iitk.ac.in/raktarpan/live/', $output);
-?>
